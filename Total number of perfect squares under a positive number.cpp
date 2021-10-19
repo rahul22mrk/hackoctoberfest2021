9270cbdcfecd4b88e6d2f05c5c2a557559d3d951
Ahamed-Favas <89861677+Ahamed-Favas@users.noreply.github.com>
2021-10-19 18:04:08 +0530

    Create Total number of perfect squares under a positive number.cpp

---
+++ Total number of perfect squares under a positive number.cpp
@@ -0,0 +1,11 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main ()
+{
+  int n, c = 0;
+  cout << "Enter a positive Number \n";
+  cin >> n;
+  c = floor (sqrt (n));
+  cout << "Total number of perfect squares under " << n <<" is " << c;
+
+}
